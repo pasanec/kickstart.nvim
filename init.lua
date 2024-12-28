74fa07785bf855f21483df3179421821b4445a72
Immanuel Pasanec <immanuel@pasanec.de>
2024-12-28 18:32:27 +0100

    Enabled custom.plugins and added tools for typescript.

--- init.lua
+++ init.lua
@@ -641,6 +641,7 @@ require('lazy').setup({
             },
           },
         },
+        intelephense = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -937,7 +938,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
